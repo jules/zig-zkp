db005ae83198304793c8edd528540569127ecb97
Jules de Smit <julesdesmit@gmail.com>
2023-11-16 02:31:32 +0100

    Fixes

--- src/poly.zig
+++ src/poly.zig
@@ -1,218 +1,222 @@
 const std = @import("std");
 const Allocator = std.mem.Allocator;
+const ArrayList = std.ArrayList;
 
-const PolynomialError = error {
+const PolynomialError = error{
     NoQuotientValue,
 };
 
-const QuoRem = struct {
-    quotient: Polynomial,
-    remainder: Polynomial,
-};
+fn QuoRem(comptime T: type) type {
+    return struct {
+        quotient: Polynomial(T),
+        remainder: Polynomial(T),
+    };
+}
 
-/// Defines a polynomial with any kind of field element.
+/// Defines a univariate polynomial with any kind of field element.
 /// Makes no distinction between dense or sparse populations but tries to be as efficient
 /// as possible despite this.
 ///
 /// Coefficients are considered to be rising in degree.
 /// XXX: guards for empty polys
-pub const Polynomial = struct{
-    elements: ArrayList(anytype),
+pub fn Polynomial(comptime T: type) type {
+    return struct {
+        elements: ArrayList(T),
 
-    pub fn new(elements: ArrayList(anytype)) Polynomial {
-        return Polynomial{
-            .elements: elements,
+        pub fn new(elements: ArrayList(T)) Polynomial(T) {
+            return Polynomial(T){
+                .elements = elements,
+            };
         }
-    }
 
-    pub fn isZero(self: Polynomial) bool {
-        return self.degree() == 0;
-    }
+        pub fn isZero(self: Polynomial(T)) bool {
+            return self.degree() == 0;
+        }
 
-    pub fn degree(self: Polynomial) usize {
-        var max_index: isize = -1;
-        for (i, v) |0.., self| {
-            if !v.isZero() {
-                max_index = i;
+        pub fn degree(self: Polynomial(T)) usize {
+            var max_index: isize = -1;
+            for (0.., self) |i, v| {
+                if (!v.isZero()) {
+                    max_index = i;
+                }
             }
-        }
-        return i;
-    }
-
-    pub fn neg(self: Polynomial) Polynomial {
-        var new_value = self;
-        for (v) |&new_value| {
-            v.negAssign();
+            return max_index;
         }
 
-        return new_value;
-    }
-
-    pub fn negAssign(self: *Polynomial) void {
-        for (v) |&self| {
-            v.negAssign();
-        }
-    }
-
-    inline fn addInner(value: *Polynomial, other: Polynomial) {
-        const m = @max(value.elements.length(), other.elements.length());
-        if m > value.elements.length() {
-            value.elements.extend(m - value.elements.length());
-        }
-
-        for (i, v1, v2) |(0.., value.elements, other.elements)| {
-            value[i] = v1.add(v2);
-        }
-    }
-
-    pub fn add(self: Polynomial, other: Polynomial) Polynomial {
-        var new_value = self;
-        addInner(new_value, other);
-        return new_value;
-    }
-
-    pub fn addInner(self: *Polynomial, other: Polynomial) void {
-        addInner(self, other);
-    }
-
-    pub fn sub(self: Polynomial, other: Polynomial) Polynomial {
-        const new_other = other.neg();
-        addInner(new_value, new_other);
-        return new_value;
-    }
-
-    pub fn subInner(self: *Polynomial, other: Polynomial) void {
-        const new_other = other.neg();
-        addInner(self, new_other);
-    }
-
-    inline fn mulInner(value: Polynomial, other: Polynomial, allocator: Allocator) Polynomial {
-        const length = value.elements.length() + other.elements.length() - 1;
-        var list = ArrayList(anytype).init(allocator);
-        list.appendNTimes(anytype.new(0), length);
-        var result = Polynomial{
-            .elements: list,
-        };
-
-        for (i, v1) |0.., value.elements| {
-            // sparse poly optimization
-            if v1.isZero() {
-                continue;
+        pub fn neg(self: Polynomial(T)) Polynomial(T) {
+            var new_value = self;
+            for (&new_value) |*v| {
+                v.negAssign();
             }
 
-            for (j, v2) |0.., other.elements| {
-                result.elements[i+j].addAssign(v1.mul(v2));
+            return new_value;
+        }
+
+        pub fn negAssign(self: *Polynomial(T)) void {
+            for (&self) |*v| {
+                v.negAssign();
             }
         }
 
-        return result;
-    }
-
-    pub fn mul(self: Polynomial, other: Polynomial, allocator: Allocator) Polynomial {
-        return mulInner(self, other);
-    }
-
-    pub fn mulAssign(self: *Polynomial, other: Polynomial, allocator: Allocator) Polynomial {
-        self = mulInner(self, other);
-    }
-
-    inline fn divInner(numerator: Polynomial, denominator: Polynomial, allocator: Allocator) ?QuoRem {
-        if denominator.degree() == -1 {
-            return null;
-        }
-    
-        if numerator.degree() < denominator.degree() {
-            return QuoRem { .quotient: Polynomial.new(ArrayList(anytype).init(allocator)), .remainder: numerator};
-        }
-
-        var remainder = numerator;
-        var q_list = ArrayList(anytype).init(allocator);
-        const size = numerator.degree()-denominator.degree()+1;
-        q_list.appendNTimes(anytype.new(0), size);
-        var quotient = Polynomial.new(q_list);
-
-        for (i) |0..size| {
-            if remainder.degree() < denominator.degree() {
-                break;
+        inline fn addInner(value: *Polynomial(T), other: Polynomial(T)) void {
+            const m = @max(value.elements.length(), other.elements.length());
+            if (m > value.elements.length()) {
+                value.elements.extend(m - value.elements.length());
             }
 
-            const coeff = remainder.leadingCoefficient().div(denominator.leadingCoefficient());
-            const shift = remainder.degree() - denominator.degree();
-
-            var sub_list = ArrayList(anytype).init(allocator);
-            sub_list.appendNTimes(anytype.new(0), shift);
-            sub_list.append(coeff);
-            const sub = Polynomial.new(sub_list);
-            defer sub.deinit();
-
-            quotient.elements[shift] = coeff;
-            remainder.subAssign(sub);
+            for (0.., value.elements, other.elements) |i, v1, v2| {
+                value[i] = v1.add(v2);
+            }
         }
 
-        return QuoRem { .quotient: quotient, .remainder: remainder };
-    }
-
-    pub fn div(self: Polynomial, other: Polynomial) ?QuoRem {
-        return divInner(self, other);
-    }
-
-    pub fn divAssign(self: *Polynomial, other: Polynomial) !void {
-        const quorem = divInner(self, other) orelse return NoQuotientValue;
-        self = quorem.quotient;
-    }
-
-    pub fn eval(self: Polynomial, point: anytype) anytype {
-        var result = self.elements[0];
-        const base_point = point;
-
-        for (v) |self.elements| {
-            result.addAssign(v.mul(point));
-            point.mulAssign(base_point);
+        pub fn add(self: Polynomial(T), other: Polynomial(T)) Polynomial(T) {
+            var new_value = self;
+            addInner(new_value, other);
+            return new_value;
         }
-    }
 
-    // Simple iterative method
-    // NOTE: should be updated to use FFT at some point
-    pub fn interpolate(domain: &[anytype], values: &[anytype], allocator: Allocator) Polynomial {
-        var x_list = ArrayList(anytype).init(allocator);
-        x_list.push(anytype.new(0));
-        x_list.push(anytype.new(1));
-        const x = Polynomial.new(x_list);
-        var acc = Polynomial.new(ArrayList(anytype).init(allocator));
-        for (i, _) |0.., domain| {
-            var product_list = ArrayList(anytype).init(allocator);
-            product_list.push(values[i]);
-            var product = Polynomial.new(product_list);
+        pub fn addAssign(self: *Polynomial(T), other: Polynomial(T)) void {
+            addInner(self, other);
+        }
 
-            for (j, _) |0.., domain| {
-                if (j == i) {
+        pub fn sub(self: Polynomial(T), other: Polynomial(T)) Polynomial(T) {
+            var new_value = self;
+            const new_other = other.neg();
+            addInner(new_value, new_other);
+            return new_value;
+        }
+
+        pub fn subInner(self: *Polynomial(T), other: Polynomial(T)) void {
+            const new_other = other.neg();
+            addInner(self, new_other);
+        }
+
+        inline fn mulInner(value: Polynomial(T), other: Polynomial(T), allocator: Allocator) Polynomial(T) {
+            const length = value.elements.length() + other.elements.length() - 1;
+            var list = ArrayList(T).init(allocator);
+            list.appendNTimes(T.new(0), length);
+            var result = Polynomial(T){
+                .elements = list,
+            };
+
+            for (0.., value.elements) |i, v1| {
+                // sparse poly optimization
+                if (v1.isZero()) {
                     continue;
                 }
 
-
-                var a_list = ArrayList(anytype).init(allocator);
-                a_list.push(domain[j]);
-                const a = Polynomial.new(a_list);
-
-                var b_list = ArrayList(anytype).init(allocator);
-                b_list.push(domain[i].sub(domain[j]));
-                var b = Polynomial.new(b_list);
-                b.negAssign();
-                product.mulAssign(x.sub(a).mul(b));
+                for (0.., other.elements) |j, v2| {
+                    result.elements[i + j].addAssign(v1.mul(v2));
+                }
             }
 
-            acc.addAssign(product);
+            return result;
         }
 
-        return acc;
-    }
+        pub fn mul(self: Polynomial(T), other: Polynomial(T), allocator: Allocator) Polynomial(T) {
+            return mulInner(self, other, allocator);
+        }
 
-    pub fn leadingCoefficient(self: Polynomial) anytype {
-        return self.elements[self.degree()];
-    }
+        pub fn mulAssign(self: *Polynomial(T), other: Polynomial(T), allocator: Allocator) Polynomial(T) {
+            self = mulInner(self, other, allocator);
+        }
 
-    pub fn deinit(self: Polynomial) void {
-        self.elements.deinit();
-    }
-};
+        inline fn divInner(numerator: Polynomial(T), denominator: Polynomial(T), allocator: Allocator) ?QuoRem(T) {
+            if (denominator.degree() == -1) {
+                return null;
+            }
 
+            if (numerator.degree() < denominator.degree()) {
+                return QuoRem(T){ .quotient = Polynomial(T).new(ArrayList(T).init(allocator)), .remainder = numerator };
+            }
+
+            var remainder = numerator;
+            var q_list = ArrayList(T).init(allocator);
+            const size = numerator.degree() - denominator.degree() + 1;
+            q_list.appendNTimes(T.new(0), size);
+            var quotient = Polynomial(T).new(q_list);
+
+            for (0..size) |_| {
+                if (remainder.degree() < denominator.degree()) {
+                    break;
+                }
+
+                const coeff = remainder.leadingCoefficient().div(denominator.leadingCoefficient());
+                const shift = remainder.degree() - denominator.degree();
+
+                var sub_list = ArrayList(T).init(allocator);
+                sub_list.appendNTimes(T.new(0), shift);
+                sub_list.append(coeff);
+                const subtractee = Polynomial(T).new(sub_list);
+                defer subtractee.deinit();
+
+                quotient.elements[shift] = coeff;
+                remainder.subAssign(subtractee);
+            }
+
+            return QuoRem(T){ .quotient = quotient, .remainder = remainder };
+        }
+
+        pub fn div(self: Polynomial(T), other: Polynomial(T)) ?QuoRem(T) {
+            return divInner(self, other);
+        }
+
+        pub fn divAssign(self: *Polynomial(T), other: Polynomial(T)) !void {
+            const quorem = divInner(self, other) orelse return PolynomialError.NoQuotientValue;
+            self = quorem.quotient;
+        }
+
+        pub fn eval(self: Polynomial(T), point: T) T {
+            var result = self.elements[0];
+            const base_point = point;
+
+            for (self.elements) |v| {
+                result.addAssign(v.mul(point));
+                point.mulAssign(base_point);
+            }
+        }
+
+        // Simple iterative method
+        // NOTE: should be updated to use FFT at some point
+        pub fn interpolate(domain: []T, values: []T, allocator: Allocator) Polynomial(T) {
+            var x_list = ArrayList(T).init(allocator);
+            x_list.push(T.new(0));
+            x_list.push(T.new(1));
+            const x = Polynomial(T).new(x_list);
+            var acc = Polynomial(T).new(ArrayList(T).init(allocator));
+            for (0.., domain) |i, _| {
+                var product_list = ArrayList(T).init(allocator);
+                product_list.push(values[i]);
+                var product = Polynomial(T).new(product_list);
+
+                for (0.., domain) |j, _| {
+                    if (j == i) {
+                        continue;
+                    }
+
+                    var a_list = ArrayList(T).init(allocator);
+                    a_list.push(domain[j]);
+                    const a = Polynomial(T).new(a_list);
+
+                    var b_list = ArrayList(T).init(allocator);
+                    b_list.push(domain[i].sub(domain[j]));
+                    var b = Polynomial(T).new(b_list);
+                    b.negAssign();
+                    product.mulAssign(x.sub(a).mul(b));
+                }
+
+                acc.addAssign(product);
+            }
+
+            return acc;
+        }
+
+        pub fn leadingCoefficient(self: Polynomial(T)) T {
+            return self.elements[self.degree()];
+        }
+
+        pub fn deinit(self: Polynomial(T)) void {
+            self.elements.deinit();
+        }
+    };
+}
